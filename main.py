5d9003a5ed6ec6b645f3d1a1d859be9eeb71612e
Yunus Emre Isikdemir <73908804+yisikdemir@users.noreply.github.com>
2023-03-28 21:41:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+#------------------------------------------------------------------------------
+# Author: Yunus Emre Işıkdemir
+# 
+# Create Date: 02/11/2021
+#
+# Project Name: Point Cloud Data Visualizer Graphical User Interface
+#
+# Description: Driver program
+# 
+#------------------------------------------------------------------------------
+
 from PyQt5.QtWidgets import QApplication
 from maps import SARSTM
 import sys
